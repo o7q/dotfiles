00b8a6c7a181bd690ce763df3271f2f7a5d36afc
James <103224608+o7q@users.noreply.github.com>
2025-11-09 01:47:25 -0600

    Add `gray_desert`

--- hyprland/desktop/blue_flowers/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-   # Commands to run in interactive sessions can go here
-   set fish_greeting
-   oh-my-posh init fish --config ~/.config/oh-my-posh/catppuccin.light-blue.omp.json | source
-end
