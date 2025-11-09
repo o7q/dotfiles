ca34ddada8c47fdb0aa68102095755e33ee10e21
James <103224608+o7q@users.noreply.github.com>
2025-11-08 18:06:58 -0600

    Add blue_flowers dotfiles

---
+++ hyprland/desktop/blue_flowers/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+   # Commands to run in interactive sessions can go here
+   set fish_greeting
+   oh-my-posh init fish --config ~/.config/oh-my-posh/catppuccin.light-blue.omp.json | source
+end
